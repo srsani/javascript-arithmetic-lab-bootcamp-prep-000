9c201eb5dd1c29b03513799bc5269442e53db9ce
droll-accelerator-1483 <srsani@gmail.com>
2019-06-19 14:19:25 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(num1, num2) {
+  return num1+num2
+
+}
+
+function subtract(num1, num2) {
+  return num1-num2
+
+}
+
+function multiply(num1, num2) {
+  return num1*num2
+
+}
+
+function divide(num1, num2) {
+  return num1/num2
+
+}
+
+function inc (num){
+  return num +=1
+  
+}
+
+function dec (num){
+  return num -=1
+  
+}
+
+function makeInt(num_str) {
+  return num_str.toString()
+  
+}
